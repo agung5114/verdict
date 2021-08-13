182fbc062698f8ea0317c9da9084a4ae9d25a81d
gaspoltech <88130246+gaspoltech@users.noreply.github.com>
2021-08-14 03:08:13 +0700

    Add files via upload

--- verdict.py
+++ verdict.py
@@ -30,61 +30,81 @@ def main():
         st.subheader("Verdict Prediction")
         # iris= Image.open('iris.png')
 
-        model= open("vmodel.pkl", "rb")
-        knn=joblib.load(model)
-
         data = st.file_uploader('Upload File PDF',type='.pdf')
         if data == None:
             st.write('Silakan Upload File dengan format pdf')
-
-        if st.button('Extract Features'):
-            # aDict = pickle.load(open("aDict.p","rb"))
+        else:
             text = getBagian(data)
             pasal = extract_pasal(text)
             df = pd.DataFrame.from_dict(pasal)
-            pslist = df.pasal.unique()
-            st.dataframe(pslist)
-            df[['pasal','drop']] = df['pasal'].str.split(' ayat',expand=True)
-            # df[['pasal', 'drop']] = df['pasal'].str.split('pasal ', expand=True)
-            #st.write(df['pasal'].unique())
-            ftlist = ['pasal 13','pasal 4','pasal 15','pasal 16','pasal 1','pasal 5','pasal 9','pasal 19','pasal 17']
-            ftmt = df[df['pasal'].isin(ftlist)]
-            ftmt = ftmt[['pasal']]
-            ftmt = ftmt['pasal'].unique()
-            # # for ps in df.pasal.tolist():
-            # #     if ps in ftlist:
-            # #         ftmt.append(ps)
-            st.write('Fitur untuk Prediksi')
-            st.dataframe(ftmt)
-        st.subheader("Features")
-        #Intializing
-        c1,c2 = st.beta_columns((1,1))
-        with c1:
-            sl = st.number_input(label="FP Lengkap",value=1,min_value=0, max_value=1, step=1)
-            sw = st.number_input(label="FP Tepat Waktu",value=1,min_value=0, max_value=1, step=1)
-            pl = st.number_input(label="Keterangan FP Sesuai",value=0,min_value=0, max_value=1, step=1)
-            dm1 = st.number_input(label="FP Diganti Dibatalkan",value=1,min_value=0, max_value=1, step=1)
-            dm2 = st.number_input(label="FP Tidak Double Kredit",value=1,min_value=0, max_value=1, step=1)
-        with c2:
-            dm0 = st.number_input(label="Lawan PKP",value=1,min_value=0, max_value=1, step=1)
-            dm3 = st.number_input(label="Lawan Disanksi",value=1,min_value=0, max_value=1, step=1)
-            dm4 = st.number_input(label="Lawan Lapor",value=1,min_value=0, max_value=1, step=1)
-            dm5 = st.number_input(label="Minta Tanggung Jawab Lawan",value=1,min_value=0, max_value=1, step=1)
-            pw = st.number_input(label="PPN telah dibayar",value=0,min_value=0, max_value=1, step=1)
-
-        if st.button("Click Here to Classify"):
-            dfvalues = pd.DataFrame(list(zip([sl],[sw],[pl],[pw])),columns =['lengkap', 'tepatwaktu', 'ketsesuai', 'adapembayaran'])
-            input_variables = np.array(dfvalues[['lengkap', 'tepatwaktu', 'ketsesuai', 'adapembayaran']])
-            prediction = knn.predict(input_variables)
-            if prediction == 'ditolak':
-                st.subheader('Prediksi Hasil Verdict')
-                st.title('Permohonan Banding Ditolak')
-            elif prediction =='sebagian':
-                st.subheader('Prediksi Hasil Verdict')
-                st.title('Permohonan Banding Diterima Sebagian')
-            else:
-                st.subheader('Prediksi Hasil Verdict')
-                st.title('Permohonan Banding Diterima Seluruhnya')
+            tlist = ['pasal 13', 'pasal 4', 'pasal 16', 'pasal 1', 'pasal 5', 'pasal 9', 'pasal 19', 'pasal 17']
+            v1 = 1 if tlist[0] in df['pasal'].values else 0
+            v2 = 1 if tlist[1] in df['pasal'].values else 0
+            v3 = 0
+            v4 = 0
+            v5 = 1 if tlist[2] in df['pasal'].values else 0
+            v6 = 1 if tlist[3] in df['pasal'].values else 0
+            v7 = 1 if tlist[4] in df['pasal'].values else 0
+            v8 = 1 if tlist[5] in df['pasal'].values else 0
+            v9 = 1 if tlist[6] in df['pasal'].values else 0
+            v10 = 1 if tlist[7] in df['pasal'].values else 0
+            if st.button('Extract Features'):
+                # aDict = pickle.load(open("aDict.p","rb"))
+                c1,c2= st.beta_columns((3,8))
+                with c1:
+                    pslist = df.pasal.unique()
+                    st.write('Pasal yang disebut dalam pokok sengketa')
+                    df1 = df['pasal'].unique()
+                    # st.dataframe(df1.assign(hack='').set_index('hack'))
+                    st.dataframe(df1)
+                    df[['pasal','drop']] = df['pasal'].str.split(' ayat',expand=True)
+                    ftlist = ['pasal 13','pasal 4','pasal 16','pasal 1','pasal 5','pasal 9','pasal 19','pasal 17']
+                    ftmt = df[df['pasal'].isin(ftlist)]
+                    ftmt = ftmt[['pasal']]
+                    st.write('Pasal yang menjadi Fitur untuk Prediksi')
+                    st.write(ftmt['pasal'].unique())
+                with c2:
+                    ket = pd.read_csv('verdict_fitur.csv', sep=";")
+                    my_expander = st.beta_expander(label='Keterangan dan Contoh')
+                    with my_expander:
+                    # st.beta_expander('Keterangan dan Contoh Kasus berdasarkan pasal')
+                        ket = ket[ket['pasal'].isin(ftmt['pasal'])]
+                        opsi = ket['pasal_terkait'].unique()
+                        # pilihan = st.selectbox('Keterangan per pasal',list(opsi))
+                        # st.write(ket)
+                        # ftmt = ftmt.merge(ket, on='pasal', how='inner')
+                        # ket = ket[ket['pasal_terkait'].isin([pilihan])]
+                        ket = ket[['pasal_terkait','keterangan', 'contoh_kasus']]
+                        st.table(ket)
+            if st.button("Jalankan Prediksi (2)"):
+                # model= open("vmodel.pkl", "rb")
+                model = open("model_verdict/ver_knn_2.pkl", "rb")
+                knn=joblib.load(model)
+                dfvalues = pd.DataFrame(list(zip([v1],[v2],[v3],[v4],[v5],[v6],[v7],[v8],[v9],[v10])),columns =['pasal 13', 'pasal 4','pasal 15','pasal 16A', 'pasal 16B', 'pasal 1', 'pasal 5', 'pasal 9', 'pasal 19', 'pasal 17'])
+                input_variables = np.array(dfvalues[['pasal 13', 'pasal 4','pasal 15','pasal 16A', 'pasal 16B', 'pasal 1', 'pasal 5', 'pasal 9', 'pasal 19', 'pasal 17']])
+                prediction = knn.predict(input_variables)
+                if prediction == 0:
+                    st.subheader('Prediksi Hasil Verdict')
+                    st.title('Permohonan Banding Ditolak')
+                else:
+                    st.subheader('Prediksi Hasil Verdict')
+                    st.title('Permohonan Banding Dikabulkan')
+            if st.button("Jalankan Prediksi (3)"):
+                # model= open("vmodel.pkl", "rb")
+                model = open("model_verdict/ver_knn_3.pkl", "rb")
+                knn=joblib.load(model)
+                dfvalues = pd.DataFrame(list(zip([v1],[v2],[v3],[v4],[v5],[v6],[v7],[v8],[v9],[v10])),columns =['pasal 13', 'pasal 4','pasal 15','pasal 16A', 'pasal 16B', 'pasal 1', 'pasal 5', 'pasal 9', 'pasal 19', 'pasal 17'])
+                input_variables = np.array(dfvalues[['pasal 13', 'pasal 4','pasal 15','pasal 16A', 'pasal 16B', 'pasal 1', 'pasal 5', 'pasal 9', 'pasal 19', 'pasal 17']])
+                prediction = knn.predict(input_variables)
+                if prediction == 0:
+                    st.subheader('Prediksi Hasil Verdict')
+                    st.title('Permohonan Banding Ditolak')
+                elif prediction ==1:
+                    st.subheader('Prediksi Hasil Verdict')
+                    st.title('Permohonan Banding Dikabulkan Sebagian')
+                else:
+                    st.subheader('Prediksi Hasil Verdict')
+                    st.title('Permohonan Banding Dikabulkan Seluruhnya')
     
     elif choice == "Dispute Prediction":
         st.title("Dispute Predicction")
